85970fdb6648a3f92deaaa005f4ae9abfb77eefb
abhishek.kumar <abhishek.kumar@mcwaneservices.com>
2017-03-23 11:06:03 +0530

    Adding bootstrap

--- app/classic_vendors.js
+++ app/classic_vendors.js
@@ -1,6 +1,8 @@
 require('../node_modules/angular-ui-router/release/angular-ui-router.min.js');
-
+require('../node_modules/angular-bootstrap/ui-bootstrap.min.js');
+require('../node_modules/angular-bootstrap/ui-bootstrap-tpls.min.js');
 export default angular.module('lighting_ui.classic_vendors', [
   'ui.router',
+  'ui.bootstrap',
   'ngMessages'
 ]);
